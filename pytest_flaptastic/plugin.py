c6c8529b9d1e8358a7b955e70d1d5a734c9e0635
Jonathan Block <block.jon@gmail.com>
2018-10-06 15:13:53 -0700

    Saving progress

--- pytest_flaptastic/plugin.py
+++ pytest_flaptastic/plugin.py
@@ -1,15 +1,28 @@
 """
 A pytest plugin which helps tracking flappy tests.
 """
-
-import pytest
+import socket
 
 # ############### pytest hooks ################
 
 
 def pytest_addoption(parser):
-    group = parser.getgroup('django')
-    group._addoption('--xxx-db',
-                     action='store_true', dest='reuse_db', default=False,
-                     help='Re-use the testing database if it already exists, '
-                          'and do not remove it when the test finishes.')
+    group = parser.getgroup('flaptastic')
+    group._addoption('--flaptastic-key',
+                     action='store', type=str, dest='flaptastic_key',
+                     default=None, help='Flaptastic private project secret')
+
+
+def pytest_runtest_makereport(item, call):
+    if call.excinfo is not None:
+        if item.session.config.getoption("flaptastic_key"):
+            send_flap(item, call)
+
+
+def send_flap(item, call):
+    key = item.session.config.getoption("flaptastic_key")
+    host = "api.flaptastic.com"
+    port = 6789
+    client_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    message = "something flapped. key = {}".format(key)
+    client_sock.sendto(message.encode(), (host, port))
