56c022d1564eec55ab83456e9860bf582cf3de8c
Jonathan Block <block.jon@gmail.com>
2018-10-06 23:11:34 -0700

    update

--- pytest_flaptastic/plugin.py
+++ pytest_flaptastic/plugin.py
@@ -2,6 +2,9 @@
 A pytest plugin which helps tracking flappy tests.
 """
 import socket
+import json
+import hashlib
+import time
 
 # ############### pytest hooks ################
 
@@ -11,6 +14,15 @@ def pytest_addoption(parser):
     group._addoption('--flaptastic-key',
                      action='store', type=str, dest='flaptastic_key',
                      default=None, help='Flaptastic private project secret')
+    group._addoption('--commit-id',
+                     action='store', type=str, dest='commit_id',
+                     default=None, help='Source control commit id (git sha)')
+    group._addoption('--branch',
+                     action='store', type=str, dest='branch',
+                     default=None, help='Branch name under test')
+    group._addoption('--flaptastic-secret',
+                     action='store', type=str, dest='flaptastic_secret',
+                     default=None, help='Secret key for flaptastic')
 
 
 def pytest_runtest_makereport(item, call):
@@ -21,8 +33,31 @@ def pytest_runtest_makereport(item, call):
 
 def send_flap(item, call):
     key = item.session.config.getoption("flaptastic_key")
+    commit_id = item.session.config.getoption("commit_id")
+    branch = item.session.config.getoption("branch")
+    flaptastic_secret = item.session.config.getoption("flaptastic_secret")
     host = "api.flaptastic.com"
     port = 6789
     client_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    message = "something flapped. key = {}".format(key)
-    client_sock.sendto(message.encode(), (host, port))
+    payload = {
+        "exception": str(call.excinfo.value),
+        "file": item.location[0],
+        "name": item.name,
+        "line": call.excinfo.traceback[len(call.excinfo.traceback)-1].lineno,
+        "commit_id": commit_id,
+        "branch": branch
+    }
+    ts = int(time.time())
+    document = {
+        "key": key,
+        "timestamp": ts,
+        "signature": hashlib.md5(
+            "{}-{}-{}".format(
+                flaptastic_secret,
+                ts,
+                json.dumps(payload)
+            ).encode()
+        ).hexdigest(),
+        "payload": payload
+    }
+    client_sock.sendto(json.dumps(document).encode(), (host, port))
--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -1,2 +1,4 @@
 def test_simple_assertion():
+    x = 1
+    y = 2
     assert True is False
