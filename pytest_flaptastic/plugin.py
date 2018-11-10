a5dbcdd91158ac5df5c7d393d2512e92b763e0fc
Jonathan Block <block.jon@gmail.com>
2018-11-10 10:48:10 -0800

    updates

--- pytest_flaptastic/plugin.py
+++ pytest_flaptastic/plugin.py
@@ -11,45 +11,62 @@ import time
 
 def pytest_addoption(parser):
     group = parser.getgroup('flaptastic')
-    group._addoption('--flaptastic-key',
-                     action='store', type=str, dest='flaptastic_key',
-                     default=None, help='Flaptastic private project secret')
-    group._addoption('--commit-id',
+    group._addoption('--flaptastic-account-id',
+                     action='store', type=str, dest='flaptastic_organization_id',
+                     default=None, help='Flaptastic organization id')
+    group._addoption('--flaptastic-api-token',
+                     action='store', type=str, dest='flaptastic_api_token',
+                     default=None, help='Flaptastic api token')
+    group._addoption('--flaptastic-commit-id',
                      action='store', type=str, dest='commit_id',
                      default=None, help='Source control commit id (git sha)')
-    group._addoption('--branch',
+    group._addoption('--flaptastic-branch',
                      action='store', type=str, dest='branch',
                      default=None, help='Branch name under test')
     group._addoption('--flaptastic-secret',
                      action='store', type=str, dest='flaptastic_secret',
                      default=None, help='Secret key for flaptastic')
+    group._addoption('--flaptastic-microservice',
+                     action='store', type=str, dest='flaptastic_microservice',
+                     default=None, help='Identifier for microservice under test')
+    group._addoption('--flaptastic-link',
+                     action='store', type=str, dest='flaptastic_link',
+                     default=None, help='Optional link to CI page with full details')
 
 
 def pytest_runtest_makereport(item, call):
     if call.excinfo is not None:
-        if item.session.config.getoption("flaptastic_key"):
+        if item.session.config.getoption("flaptastic_organization_id"):
             send_flap(item, call)
 
 
 def send_flap(item, call):
-    key = item.session.config.getoption("flaptastic_key")
+    organization_id = item.session.config.getoption("flaptastic_organization_id")
+    api_token = item.session.config.getoption("flaptastic_api_token")
     commit_id = item.session.config.getoption("commit_id")
     branch = item.session.config.getoption("branch")
     flaptastic_secret = item.session.config.getoption("flaptastic_secret")
+    flaptastic_microservice = item.session.config.getoption("flaptastic_microservice")
+    flaptastic_link = item.session.config.getoption("flaptastic_link")
     host = "api.flaptastic.com"
     port = 6789
     client_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     payload = {
-        "exception": str(call.excinfo.value),
-        "file": item.location[0],
-        "name": item.name,
-        "line": call.excinfo.traceback[len(call.excinfo.traceback)-1].lineno,
+        "microservice": flaptastic_microservice,
         "commit_id": commit_id,
         "branch": branch
+        "link": flaptastic_link,
+        "test_results": {
+            "exception": str(call.excinfo.value),
+            "file": item.location[0],
+            "name": item.name,
+            "line": call.excinfo.traceback[len(call.excinfo.traceback)-1].lineno,
+        }
     }
     ts = int(time.time())
     document = {
-        "key": key,
+        "organization_id": organization_id,
+        "api_token": api_token,
         "timestamp": ts,
         "signature": hashlib.md5(
             "{}-{}-{}".format(
