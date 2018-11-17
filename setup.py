0117949623a318e47001e58b377d341e3cb58627
Jonathan Block <block.jon@gmail.com>
2018-11-17 03:24:44 -0800

    Better env vars

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pytest-flaptastic",
-    version="0.0.4",
+    version="0.0.5",
     author="Jonathan Block",
     author_email="block.jon@gmail.com",
     description="Flaptastic py.test plugin",
